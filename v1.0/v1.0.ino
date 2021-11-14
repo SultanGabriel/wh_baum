866eb8cc3ade923ca15617f4ae6919db24f78320
Sultan <sultangabriel75@yahoo.com>
2021-11-14 20:27:11 +0100

    added more functionality to LCD MENU .. still gotta figure a way to compress everything in a class.

--- v1.0/v1.0.ino
+++ v1.0/v1.0.ino
@@ -5,17 +5,6 @@
 // Custom chars
 /********************/
 
-
-byte Heart[] = {
-  B00000,
-  B01010,
-  B11111,
-  B11111,
-  B01110,
-  B00100,
-  B00000,
-  B00000
-};
 byte Speed[] = {
   B00000,
   B00100,
@@ -67,13 +56,23 @@ byte Sound[] = {
   B11000
 };
 byte NOT_DEFINED[8] = {
+  B00000,
+  B01110,
+  B01010,
+  B00010,
+  B00110,
+  B00100,
+  B00100,
+  B00000
+};
+byte NOT_DEFINED_INVERTED[8] = {
   B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
-  B11111,
+  B10001,
+  B10101,
+  B11101,
+  B11001,
+  B11011,
+  B11011,
   B11111
 };
 
@@ -87,14 +86,18 @@ byte NOT_DEFINED[8] = {
           LCD // Wiring: SDA pin is connected to A4 and SCL pin to A5.
 */
 
+int butLeft = 2;
+int butRight = 3;
+int butSelect = 4;
+
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 
 void setup() {
   // init pins
 
-  pinMode(1, INPUT);  // LEFT
-  pinMode(2, INPUT);  // RIGHT
-  pinMode(3, INPUT);  // SELECT
+  pinMode(butLeft, INPUT);  // LEFT
+  pinMode(butRight, INPUT);  // RIGHT
+  pinMode(butSelect, INPUT);  // SELECT
 
   // init display and custom chars
 
@@ -102,31 +105,103 @@ void setup() {
 
   lcd.init();
   lcd.backlight();
-  lcd.cursor();
+  lcd.noCursor();
 
   // -------INIT Chars
 
-sss
-  lcd.createChar(1, Heart);
-  lcd.createChar(2, NOT_DEFINED);
-  lcd.createChar(3, Check);
+  lcd.createChar(0, Speed);
+  lcd.createChar(1, NOT_DEFINED);
+  lcd.createChar(2, Check);
+  lcd.createChar(3, NOT_DEFINED);
+
+  // INVERTED
+
   lcd.createChar(4, Speed_Inverted);
-  lcd.createChar(5, NOT_DEFINED);
-  lcd.createChar(6, NOT_DEFINED);
-  lcd.createChar(7, Check_Inverted);
+  lcd.createChar(5, NOT_DEFINED_INVERTED);
+  lcd.createChar(6, Check_Inverted);
+  lcd.createChar(7, NOT_DEFINED_INVERTED);
 
   //----------
 
-
+  Serial.begin(9600);
 
 }
 int lcd_cursorPos = 0;
+bool selected = false;
+
+unsigned long millsPress;
+
+int leds_speed = 100;
+
+void lcd_select() {
+
+}
+
 
 void lcd_update() {
-  lcd.setCursor(2, 0);
-  lcd.print("Hello World!");
 
-  lcd_drawMenu();
+  if (!selected) {
+    if (digitalRead(2) == 1 && millis() - millsPress > 200) {
+      millsPress = millis();
+      //    Serial.write("LEFT\n");
+
+      if (lcd_cursorPos == 0) {
+        lcd_cursorPos = 3;
+      } else {
+        lcd_cursorPos = (lcd_cursorPos - 1) % 4;
+      }
+
+      lcd_drawMenu();
+    }
+
+    if (digitalRead(3) == 1 && millis() - millsPress > 200) {
+      millsPress = millis();
+      //   Serial.write("RIGHT\n");
+      lcd_cursorPos = (lcd_cursorPos + 1) % 4;
+
+      lcd_drawMenu();
+    }
+  }
+  if (digitalRead(4) == 1 && millis() - millsPress > 200) {
+    millsPress = millis();
+
+    selected = !selected;
+
+    lcd_drawMenu();
+    //    Serial.write(lcd_cursorPos);
+    //    Serial.write("SELECT\n");
+  }
+
+  /* --- --- --- --- --- --- --- --- --- --- --- --- ---*/
+
+  if (selected) {
+    switch (lcd_cursorPos) {
+      case 0:
+        Serial.write("pos = 0");
+
+        lcd.setCursor(1, 0);
+        if (digitalRead(2) == 1 && leds_speed != 0 && millis() - millsPress > 200) {
+          millsPress = millis();
+
+          leds_speed = leds_speed - 2;
+        } else if (digitalRead(3) == 1 && leds_speed != 100 && millis() - millsPress > 200) {
+          millsPress = millis();
+
+          leds_speed = leds_speed + 2;
+        }
+
+        lcd.print("1. Speed: " + String(leds_speed) + "%");
+        break;
+      case 1:
+        break;
+      case 2:
+        break;
+      case 3:
+        break;
+    }
+  }
+
+
 
   /*  int c = 0;
     for (int pos = 0; pos < 16; pos += 2) {
@@ -137,10 +212,48 @@ void lcd_update() {
     }*/
 }
 
+
 void lcd_drawMenu() {
+  bool invert = true;
   int c = 0;
+  unsigned long millsSel;
+
+
+
+  if (!selected) {
+    switch (lcd_cursorPos) {
+      case 0:
+        lcd.clear();
+        lcd.setCursor(1, 0);
+        lcd.print("1. Speed");
+        break;
+
+      case 1:
+        lcd.clear();
+        lcd.setCursor(1, 0);
+        lcd.print("2. Modes");
+        break;
+
+      case 2:
+        lcd.clear();
+        lcd.setCursor(1, 0);
+        lcd.print("3. RGB" );
+        break;
+
+      case 3:
+        lcd.clear();
+        lcd.setCursor(1, 0);
+        lcd.print("4. Back");
+        break;
+
+        //default:
+        //lcd.write("");
+    }
+  }
+  lcd.setCursor(0, 1);
+
   for (int pos = 0; pos < 8; pos += 2) {
-    if (lcd_cursorPos * 2 == pos) {
+    if (lcd_cursorPos * 2 == pos && invert) {
       lcd_drawChar(pos, 1, c + 4);
     } else {
       lcd_drawChar(pos, 1, c);
@@ -149,15 +262,18 @@ void lcd_drawMenu() {
       c = c + 1;
     }
   }
+
+  lcd.setCursor(14, 1);
+  lcd.print(lcd_cursorPos);
 }
 
 void lcd_drawChar(int x, int y, int c) {
   // POSITION X , Y ------ custom character code 0-7
-
   lcd.setCursor(x, y);
   lcd.write(c);
 }
 
 void loop() {
   lcd_update();
+
 }
