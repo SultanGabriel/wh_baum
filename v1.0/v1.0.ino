c25c3acd5f337f7f0ffcb3c00abf7d2b937cb4ae
Sultan <sultan7575@protonmail.com>
2021-11-15 23:08:49 +0100

    worked on new symbols for LCD and added a pressDelay variable

--- v1.0/v1.0.ino
+++ v1.0/v1.0.ino
@@ -1,12 +1,35 @@
-#include <Wire.h> // Library for I2C communication
+#include <Wire.h>              // Library for I2C communication
 #include <LiquidCrystal_I2C.h> // Library for LCD
-
 
 // Custom chars
 /********************/
 
 byte Speed[] = {
   B00000,
+  B10100,
+
+  B01010,
+  B00101,
+
+  B00101,
+  B01010,
+
+  B10100,
+  B00000
+};
+byte Speed_Inverted[] {
+  B11111,
+  B01011,
+  B10101,
+  B11010,
+  B11010,
+  B10101,
+  B01011,
+  B11111
+};
+
+byte IDK[] = {
+  B00000,
   B00100,
   B01010,
   B10001,
@@ -15,7 +38,7 @@ byte Speed[] = {
   B00100,
   B00000,
 };
-byte Speed_Inverted[] = {
+byte IDK_Inverted[] = {
   B11111,
   B11011,
   B10101,
@@ -25,6 +48,7 @@ byte Speed_Inverted[] = {
   B11011,
   B11111,
 };
+
 byte Check[] = {
   B00000,
   B00001,
@@ -45,44 +69,45 @@ byte Check_Inverted[] = {
   B11111,
   B11111
 };
-byte Sound[] = {
-  B00001,
-  B00011,
-  B00101,
-  B01001,
-  B01001,
-  B01011,
-  B11011,
-  B11000
-};
+
 byte NOT_DEFINED[8] = {
-  B00000,
   B01110,
-  B01010,
+  B10001,
+  B00001,
   B00010,
-  B00110,
   B00100,
+  B00000,
   B00100,
   B00000
 };
 byte NOT_DEFINED_INVERTED[8] = {
-  B11111,
   B10001,
-  B10101,
+  B01110,
+  B11110,
   B11101,
-  B11001,
   B11011,
+  B11111,
   B11011,
   B11111
 };
 
+byte Bulb[8] = {
+  B01110,
+  B10001,
+  B10001,
+  B10001,
+  B01010,
+  B01010,
+  B00100,
+  B00000
+};
 /******************/
 
 /*  TODO
       -> connect
           - LCD
           - 74HC595
-    
+
           LCD // Wiring: SDA pin is connected to A4 and SCL pin to A5.
 */
 
@@ -95,9 +120,9 @@ LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x27, 16, 2);
 void setup() {
   // init pins
 
-  pinMode(leftButtonPin, INPUT);  // LEFT
+  pinMode(leftButtonPin, INPUT);   // LEFT
   pinMode(rightButtonPin, INPUT);  // RIGHT
-  pinMode(selectButtonPin, INPUT);  // SELECT
+  pinMode(selectButtonPin, INPUT); // SELECT
 
   // init display and custom chars
 
@@ -110,39 +135,41 @@ void setup() {
   // -------INIT Chars
 
   lcd.createChar(0, Speed);
-  lcd.createChar(1, NOT_DEFINED);
-  lcd.createChar(2, Check);
-  lcd.createChar(3, NOT_DEFINED);
+  lcd.createChar(1, Bulb);
+  lcd.createChar(2, IDK);
+  lcd.createChar(3, Check);
 
   // INVERTED
 
   lcd.createChar(4, Speed_Inverted);
   lcd.createChar(5, NOT_DEFINED_INVERTED);
-  lcd.createChar(6, Check_Inverted);
-  lcd.createChar(7, NOT_DEFINED_INVERTED);
+  lcd.createChar(6, IDK_Inverted);
+  lcd.createChar(7, Check_Inverted);
 
   //----------
 
+  lcd_drawMenu();
+
   Serial.begin(9600);
-
 }
-int lcd_cursorPos = 0;
-bool selected = false;
 
+int lcd_cursorPos = 0;
+
+bool selected = false;
 unsigned long millsPress;
+int pressDelay = 250;
 
 int leds_speed = 100;
 
 void lcd_select() {
-
 }
-
 
 void lcd_update() {
   // Button Press Handler
+  // - -- --- --- -- -
   // LEFT
   if (!selected) {
-    if (digitalRead(leftButtonPin) == 1 && millis() - millsPress > 200) {
+    if (digitalRead(leftButtonPin) == 1 && millis() - millsPress > pressDelay) {
       millsPress = millis();
       //    Serial.write("LEFT\n");
 
@@ -154,9 +181,9 @@ void lcd_update() {
 
       lcd_drawMenu();
     }
-    
+
     // RIGHT
-    if (digitalRead(rightButtonPin) == 1 && millis() - millsPress > 200) {
+    if (digitalRead(rightButtonPin) == 1 && millis() - millsPress > pressDelay) {
       millsPress = millis();
       //   Serial.write("RIGHT\n");
       lcd_cursorPos = (lcd_cursorPos + 1) % 4;
@@ -166,7 +193,7 @@ void lcd_update() {
   }
 
   // Select
-  if (digitalRead(selectButtonPin) == 1 && millis() - millsPress > 200) {
+  if (digitalRead(selectButtonPin) == 1 && millis() - millsPress > pressDelay) {
     millsPress = millis();
 
     selected = !selected;
@@ -177,18 +204,18 @@ void lcd_update() {
   }
 
   /* --- --- --- --- --- --- --- --- --- --- --- --- ---*/
-  // print information about selected option + control 
+  // print information about selected option + control
   if (selected) {
     switch (lcd_cursorPos) {
       case 0: // SPEED - speed of leds blinking or fading - 0%-100%
         Serial.write("pos = 0");
 
         lcd.setCursor(1, 0);
-        if (digitalRead(leftButtonPin) == 1 && leds_speed != 0 && millis() - millsPress > 200) {
+        if (digitalRead(leftButtonPin) == 1 && leds_speed != 0 && millis() - millsPress > pressDelay) {
           millsPress = millis();
 
           leds_speed = leds_speed - 2;
-        } else if (digitalRead(rightButtonPin) == 1 && leds_speed != 100 && millis() - millsPress > 200) {
+        } else if (digitalRead(rightButtonPin) == 1 && leds_speed != 100 && millis() - millsPress > pressDelay) {
           millsPress = millis();
 
           leds_speed = leds_speed + 2;
@@ -196,7 +223,7 @@ void lcd_update() {
 
         lcd.print("1. Speed: " + String(leds_speed) + "%");
         break;
-        
+
       case 1:
         break;
       case 2:
@@ -206,8 +233,6 @@ void lcd_update() {
     }
   }
 
-
-
   /*  int c = 0;
     for (int pos = 0; pos < 16; pos += 2) {
       lcd_drawChar(pos, 1, c);
@@ -216,7 +241,6 @@ void lcd_update() {
       }
     }*/
 }
-
 
 void lcd_drawMenu() {
   bool invert = true;
@@ -241,7 +265,7 @@ void lcd_drawMenu() {
       case 2:
         lcd.clear();
         lcd.setCursor(1, 0);
-        lcd.print("3. RGB" );
+        lcd.print("3. RGB");
         break;
 
       case 3:
@@ -249,9 +273,6 @@ void lcd_drawMenu() {
         lcd.setCursor(1, 0);
         lcd.print("4. Back");
         break;
-
-        //default:
-        //lcd.write("");
     }
   }
   lcd.setCursor(0, 1);
@@ -279,5 +300,4 @@ void lcd_drawChar(int x, int y, int c) {
 
 void loop() {
   lcd_update();
-
 }
